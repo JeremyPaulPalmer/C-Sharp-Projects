10e141df35d5d34317ea108ba56196685cf50e73
Paseo92 <duxfan18@gmail.com>
2020-06-17 03:49:28 -0700

    Add Accounting Assignment
    Added Accounting Assignment project

---
+++ AccountingAssignment/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems \r\nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+        }
+}
