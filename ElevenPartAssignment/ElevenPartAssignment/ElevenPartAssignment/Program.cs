05279140365d8748cdad5f5dfdb1e6e20fc581fd
Paseo92 <duxfan18@gmail.com>
2020-06-21 13:23:00 -0700

    Update ElevenPartAssginment
    Competed 11 Part Assignment.

--- ElevenPartAssignment/ElevenPartAssignment/ElevenPartAssignment/Program.cs
+++ ElevenPartAssignment/ElevenPartAssignment/ElevenPartAssignment/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 class Program
 {
     static void Main(string[] args)
@@ -36,29 +35,74 @@ class Program
 
         List<string> names = new List<string>() { "Jeremy", "Jesse", "Susan", "Susanne", "Caliel", "Bob", "Jesus", "Javier", "Jimmy", "Jim" };
         Console.WriteLine("Please enter a name:");
-        string inputName = Console.ReadLine();
+        string inputName = Console.ReadLine(); 
         bool correctName = false;
 
-        
         do
         {
-            foreach (string name in names)
+            for (int name = 0; name < names.Count; name++)
             {
-                if (name == inputName)
+                if (names[name] == inputName)
                 {
-                    Console.WriteLine(name + " is on the list.");
+                    Console.WriteLine(names[name] + " is on the list.");
                     correctName = true;
                     break;
                 }
-
-                else if (name != inputName)
-                {
-                    Console.WriteLine(inputName + " is not on the list. Please enter another name.");
-                    inputName = Console.ReadLine();
-                }
+            }
+            if (correctName == true)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine(inputName + " is not on the list. Please enter another name.");
+                inputName = Console.ReadLine();
             }
         }
-        while (!correctName);
+        while (correctName != true);
+        Console.ReadLine();
+
+        List<string> animals = new List<string>() { "bird", "cow", "goose", "bird", "cat", "dog", "pig", "horse", "dog", "horse" };
+        //Console.WriteLine("Please enter an animal:");
+        //string inputAnimal = Console.ReadLine();
+        //bool correctAnimal = false;
+
+        //do
+        //{
+        //    for (int animal = 0; animal < animals.Count; animal++)
+        //    {
+        //        if (animals[animal] == inputAnimal)
+        //        {
+        //            Console.WriteLine(animals[animal] + " is located at index " + animal);
+        //            correctAnimal = true;
+        //        }
+        //    }
+        //    if (correctAnimal == true)
+        //    {
+        //        break;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(inputAnimal + " is not on the list. Please enter another animal.");
+        //        inputAnimal = Console.ReadLine();
+        //    }
+        //}
+        //while (correctAnimal != true);
+
+        List<string> animals2 = new List<string>();
+        foreach (string animal in animals)
+        {
+            if (!animals2.Contains(animal))
+            {
+                animals2.Add(animal);
+                Console.WriteLine(animal);
+            }
+            else if (animals2.Contains(animal))
+            {
+                Console.WriteLine(animal + " has already been listed.");
+            }
+
+        }
         Console.ReadLine();
     }
 }
