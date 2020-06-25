d6ce67f763a8675ab2157a49a716f93a1fcbad1b
Paseo92 <duxfan18@gmail.com>
2020-06-25 10:57:48 -0700

    C Sharp Integer Assginment
    Adding C Sharp Integer Assignment from Step 161 to help with debug

---
+++ IntegerAssignment/IntegerAssignment/IntegerAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a whole number to perform math operations on:");
+            ThreeMathMethods.Addition();
+        }
+    }
+}
---
+++ IntegerAssignment/IntegerAssignment/IntegerAssignment/ThreeMathMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerAssignment
+{
+    public class ThreeMathMethods
+    {
+        int userInt = Convert.ToInt32(Console.ReadLine());
+        public static void Addition()
+        {
+            int addInt = userInt + userInt;
+        }
+    }
+}
