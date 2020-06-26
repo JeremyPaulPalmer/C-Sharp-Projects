23ef604a9e41a70ba181972573551af9d72061e9
Paseo92 <duxfan18@gmail.com>
2020-06-26 12:23:12 -0700

    Add MethodOverload project
    Add MethodOverload project with MethodOverload assignment

---
+++ MethodOverload/MethodOverload/MethodOverload/Overload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Overload
+    {
+        public void sameName(int staticInt, out int addInt)
+        {
+            addInt = staticInt + 29;
+        }
+
+        public void sameName(decimal staticDec, out int decInt)
+        {
+            decimal myDec = 19.75M;
+            decimal nextDec = staticDec - myDec;
+            decInt = Convert.ToInt32(nextDec);
+        }
+
+        public void sameName(string staticStr, out int strInt)
+        {
+            int myInt = 63;
+            int nextInt = Convert.ToInt32(staticStr);
+            strInt = nextInt * myInt;
+            
+        }
+    }
+}
---
+++ MethodOverload/MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int staticInt = 53;
+            decimal staticDec = 57.25M;
+            string staticStr = "3";
+            Overload myOverload = new Overload();
+            myOverload.sameName(staticInt, out int addInt);
+            Console.WriteLine(staticInt + " plus 29 equals " + addInt + ".");
+            myOverload.sameName(staticDec, out int decInt);
+            Console.WriteLine(staticDec + " minus 19.75 equals " + decInt + ".");
+            myOverload.sameName(staticStr, out int strInt);
+            Console.WriteLine(staticStr + " multiplied by 63 equals " + strInt + ".");
+            Console.ReadLine();
+        }
+    }
+}
