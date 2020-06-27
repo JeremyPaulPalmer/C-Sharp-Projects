941656443b8137b43e4867fe35cdd1916f8440f0
Paseo92 <duxfan18@gmail.com>
2020-06-27 06:38:15 -0700

    Add TwoParameters project
    Adding TwoParameters project with Assignment C#, Step 166

---
+++ TwoParameters/TwoParameters/TwoParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 25;
+            int int2 = 32;
+            VoidMethod myVoidMethod = new VoidMethod();
+            myVoidMethod.TwoIntegers(int1, int2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoParameters/TwoParameters/TwoParameters/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class VoidMethod
+    {
+        public void TwoIntegers (int int1, int int2)
+        {
+            int int3 = int1 + 65;
+            Console.WriteLine("The first number, " + int1 + ", plus 65 equals " + int3 + ".");
+            Console.WriteLine("The second nuber is " + int2 + ".");
+        }
+    }
+}
