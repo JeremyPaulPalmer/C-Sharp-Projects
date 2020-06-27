661de6fe70e6bbe24635b407c98c6149562da4fc
Paseo92 <duxfan18@gmail.com>
2020-06-26 18:36:08 -0700

    Add TwoIntegersMethod project
    Adding TwoIntegersMethod project with assignment from C# Step 165

---
+++ TwoIntegersMethod/TwoIntegersMethod/TwoIntegersMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegersMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            int userInt1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second whole number (optional):");
+
+            TwoIntegers myTwoIntegers = new TwoIntegers();
+            int userInt2;
+            int result;
+            if(int.TryParse(Console.ReadLine(), out userInt2))
+            {
+                result = myTwoIntegers.TwoInts(userInt1, userInt2);
+            }
+            else
+            {
+                result = myTwoIntegers.TwoInts(userInt1);
+            }
+            
+            Console.WriteLine(userInt1 + " plus " + userInt2 + " plus 65 equals " + result + ".");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoIntegersMethod/TwoIntegersMethod/TwoIntegersMethod/TwoIntegers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegersMethod
+{
+    class TwoIntegers
+    {
+        public int TwoInts(int userInt1, int userInt2 = 0)
+        {
+            return userInt1 + userInt2 + 65;
+        }
+    }
+}
