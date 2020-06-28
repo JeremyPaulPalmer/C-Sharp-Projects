6116f89dbfc8b37cb2596d2c0d52dba1892daab4
Paseo92 <duxfan18@gmail.com>
2020-06-28 14:20:17 -0700

    Add SampleStudent project
    I have no idea why I named it that lol, but this contains the SayName assignment

---
+++ SampleStudent/SampleStudent/SampleStudent/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStudent
+{
+    public class Employee : Person
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ SampleStudent/SampleStudent/SampleStudent/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStudent
+{
+    public class Person
+    {
+        private string firstname;
+        private string lastname;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + firstname + " " + lastname);
+        }
+    }
+}
