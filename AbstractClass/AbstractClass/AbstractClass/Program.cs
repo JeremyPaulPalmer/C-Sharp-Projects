11ad007d5d6e5f398b456b4427f6c7370f7b5529
Paseo92 <duxfan18@gmail.com>
2020-06-30 05:54:22 -0700

    Add AbstractClass project
    Adding AbstractClass project with C# assignment from Step 179

---
+++ AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee();
+            newEmployee.firstName = "Sample";
+            newEmployee.lastName = "Employee";
+            newEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
