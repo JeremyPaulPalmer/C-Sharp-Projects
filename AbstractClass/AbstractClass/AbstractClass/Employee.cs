06a294b7df648a4a14bfbbee9d9116b44b3ff559
Paseo92 <duxfan18@gmail.com>
2020-07-01 12:23:35 -0700

    Update AbstractClass project
    Update AbstractClass project to include assignment from C# Step 187

--- AbstractClass/AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/AbstractClass/Employee.cs
@@ -8,6 +8,17 @@ namespace AbstractClass
 {
     public class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
         public override void SayName()
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);
@@ -16,6 +27,25 @@ namespace AbstractClass
         public void Quit()
         {
             Console.WriteLine("This is an example of calling a method from an interface.");
+        }
+
+        public static bool operator ==(Employee employeeOne, Employee employeeTwo)
+        {
+            bool status = false;
+            if (employeeOne.Id == employeeTwo.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+        public static bool operator !=(Employee employeeOne, Employee employeeTwo)
+        {
+            bool status = false;
+            if (employeeOne.Id != employeeTwo.Id)
+            {
+                status = true;
+            }
+            return status;
         }
     }
 }
--- AbstractClass/AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -13,10 +13,23 @@ namespace AbstractClass
             Employee newEmployee = new Employee();
             newEmployee.firstName = "Sample";
             newEmployee.lastName = "Employee";
+            newEmployee.Id = 0;
             newEmployee.SayName();
+
+            Employee nextEmployee = new Employee();
+            nextEmployee.firstName = "John";
+            nextEmployee.lastName = "Doe";
+            nextEmployee.Id = 1;
+            nextEmployee.SayName();
             
             IQuittable quittable = new Employee();
             quittable.Quit();
+
+            if (newEmployee.Id == nextEmployee.Id)
+                Console.WriteLine("An error has occurred. Two employees cannot have the same Employee ID.");
+
+            Console.WriteLine(newEmployee.Id);
+            Console.WriteLine(nextEmployee.Id);
             Console.ReadLine();
         }
     }
