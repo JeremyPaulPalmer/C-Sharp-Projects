69735aca9e2f4113bb3948c4a7bb00765a8bf781
Paseo92 <duxfan18@gmail.com>
2020-07-04 10:39:18 -0700

    Update AbstractClass project
    Updated AbstractClass projecto include C# assignment from Step 190

--- AbstractClass/AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/AbstractClass/Employee.cs
@@ -6,46 +6,47 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T>/* : Person, IQuittable*/
     {
         public int Id { get; set; }
+        public List<T> things { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
+        //public override bool Equals(object obj)
+        //{
+        //    return base.Equals(obj);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return base.GetHashCode();
+        //}
 
-        public override void SayName()
-        {
-            Console.WriteLine("Name: " + firstName + " " + lastName);
-        }
+        //public override void SayName()
+        //{
+        //    Console.WriteLine("Name: " + firstName + " " + lastName);
+        //}
 
-        public void Quit()
-        {
-            Console.WriteLine("This is an example of calling a method from an interface.");
-        }
+        //public void Quit()
+        //{
+        //    Console.WriteLine("This is an example of calling a method from an interface.");
+        //}
 
-        public static bool operator ==(Employee employeeOne, Employee employeeTwo)
-        {
-            bool status = false;
-            if (employeeOne.Id == employeeTwo.Id)
-            {
-                status = true;
-            }
-            return status;
-        }
-        public static bool operator !=(Employee employeeOne, Employee employeeTwo)
-        {
-            bool status = false;
-            if (employeeOne.Id != employeeTwo.Id)
-            {
-                status = true;
-            }
-            return status;
-        }
+        //public static bool operator ==(Employee employeeOne, Employee employeeTwo)
+        //{
+        //    bool status = false;
+        //    if (employeeOne.Id == employeeTwo.Id)
+        //    {
+        //        status = true;
+        //    }
+        //    return status;
+        //}
+        //public static bool operator !=(Employee employeeOne, Employee employeeTwo)
+        //{
+        //    bool status = false;
+        //    if (employeeOne.Id != employeeTwo.Id)
+        //    {
+        //        status = true;
+        //    }
+        //    return status;
+        //}
     }
 }
--- AbstractClass/AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -10,26 +10,43 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Employee newEmployee = new Employee();
-            newEmployee.firstName = "Sample";
-            newEmployee.lastName = "Employee";
-            newEmployee.Id = 0;
-            newEmployee.SayName();
-
-            Employee nextEmployee = new Employee();
-            nextEmployee.firstName = "John";
-            nextEmployee.lastName = "Doe";
-            nextEmployee.Id = 1;
-            nextEmployee.SayName();
+            Employee<string> listEmployees = new Employee<string>();
+            listEmployees.things = new List<string>() { "Jeremy", "Jesse", "Andy", "Ben" };
             
-            IQuittable quittable = new Employee();
-            quittable.Quit();
+            foreach (string i in listEmployees.things)
+            {
+                Console.WriteLine(i);
+            }
 
-            if (newEmployee.Id == nextEmployee.Id)
-                Console.WriteLine("An error has occurred. Two employees cannot have the same Employee ID.");
+            Employee<int> intEmployees = new Employee<int>();
+            intEmployees.things = new List<int>() { 18, 22, 56, 43, 99, 15 };
 
-            Console.WriteLine(newEmployee.Id);
-            Console.WriteLine(nextEmployee.Id);
+            foreach (int i in intEmployees.things)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //Employee newEmployee = new Employee();
+            //newEmployee.firstName = "Sample";
+            //newEmployee.lastName = "Employee";
+            //newEmployee.Id = 0;
+            //newEmployee.SayName();
+
+            //Employee nextEmployee = new Employee();
+            //nextEmployee.firstName = "John";
+            //nextEmployee.lastName = "Doe";
+            //nextEmployee.Id = 1;
+            //nextEmployee.SayName();
+            
+            //IQuittable quittable = new Employee();
+            //quittable.Quit();
+
+            //if (newEmployee.Id == nextEmployee.Id)
+            //    Console.WriteLine("An error has occurred. Two employees cannot have the same Employee ID.");
+
+            //Console.WriteLine(newEmployee.Id);
+            //Console.WriteLine(nextEmployee.Id);
             Console.ReadLine();
         }
     }
