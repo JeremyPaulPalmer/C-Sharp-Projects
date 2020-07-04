b8b26f6e5abab917a2fed4616e3ee784181f7a6b
Paseo92 <duxfan18@gmail.com>
2020-07-04 13:13:18 -0700

    Add Enum project folder
    Adding Enum project folder with assignment from C# Step 195

---
+++ Enum/Enum/Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    public enum daysOftheWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+                string userDay = Console.ReadLine();
+                daysOftheWeek newDay = (daysOftheWeek)Enum.Parse(typeof(daysOftheWeek), userDay);
+                Console.WriteLine("Thank you! Have a great " + userDay + "!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week or check your spelling.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
