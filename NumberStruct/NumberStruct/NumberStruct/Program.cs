0617aef04dd1a7f22628b5c197c8666f0b6cb541
Paseo92 <duxfan18@gmail.com>
2020-07-04 19:46:24 -0700

    Add NumberStruct project
    Added NumberStruct project folder with C# struct assignment from Step 200.

---
+++ NumberStruct/NumberStruct/NumberStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberStruct
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.14m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
