5f53dc1d50b3d816d8a3c1486af86e616495c90a
Paseo92 <duxfan18@gmail.com>
2020-07-09 09:14:12 -0700

    Add FileIO project folder
    Added FileIO project folder with assignment from C# Step 219

---
+++ FileIO/FileIO/FileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Jeremy Palmer\Desktop\C_Sharp_Projects\userinput.txt", text);
+            Console.WriteLine("\nYou entered:");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Jeremy Palmer\Desktop\C_Sharp_Projects\userinput.txt"));
+            Console.ReadLine();
+        }
+    }
+}
