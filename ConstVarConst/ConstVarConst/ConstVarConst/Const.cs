a0eea0299a234166da3a485303d23503efda031a
Paseo92 <duxfan18@gmail.com>
2020-07-10 10:01:42 -0700

    Add ConstVarConst project folder
    Adding ConstVarConst project folder that includes assignment from C# Step 229

---
+++ ConstVarConst/ConstVarConst/ConstVarConst/Const.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarConst
+{
+    public class Constructor
+    {
+        public Constructor(string name) : this(name, 18)
+        {
+        }
+        public Constructor(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ConstVarConst/ConstVarConst/ConstVarConst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarConst
+{
+    class Program
+    {
+        const string creator = "Jeremy";
+        static void Main(string[] args)
+        {
+            var constructor = new Constructor(creator);
+        }
+    }
+}
